06549be67ca426fc9497e55f350283df4fed196f
Lab Simulacion 2 <Lab Simulacion 2@EQUIPO-708>
2026-02-26 15:57:37 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/PorductoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaVirtualReyes.Models;
+
+namespace TiendaVirtualReyes.Controllers
+{
+    public class PorductoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var productos = new List<Producto>
+            {
+                new Producto { Id = 1,Nombre="Laptop",Precio= 3000, Stock=5 },
+                new Producto { Id = 2,Nombre="Mouse", Precio=80, Stock=5 }
+            };
+            return View(productos);
+        }
+    }
+}
