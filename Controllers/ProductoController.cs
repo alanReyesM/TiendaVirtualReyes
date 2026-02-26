f39735fe634ed36d0fb5fcc6d9afdc7dfc9b53a1
alanrm <alansebas2104@gmail.com>
2026-02-26 16:37:31 -0500

    xd

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -3,13 +3,13 @@ using TiendaVirtualReyes.Models;
 
 namespace TiendaVirtualReyes.Controllers
 {
-    public class PorductoController : Controller
+    public class ProductoController : Controller
     {
         public IActionResult Index()
         {
             var productos = new List<Producto>
             {
-                new Producto { Id = 1,Nombre="Laptop",Precio= 3000, Stock=5 },
+                new Producto { Id = 1,Nombre="Laptop",Precio= 3200, Stock=5 },
                 new Producto { Id = 2,Nombre="Mouse", Precio=80, Stock=5 }
             };
             return View(productos);
