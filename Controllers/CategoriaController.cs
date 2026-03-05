47abbd860f502af59edf68a59bf2c4c1fd1ff532
alanrm <alansebas2104@gmail.com>
2026-03-05 15:56:26 -0500

    yop

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaVirtualReyes.Models;
+namespace TiendaVirtualReyes.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categorias = new List<Categoria>
+            {
+                new Categoria { Id = 1,Nombre="mauro",Descripcion="amable" },
+                new Categoria { Id = 2,Nombre="leo", Descripcion="amable" },
+                new Categoria { Id = 0,Nombre="", Descripcion="" }
+            };
+            return View(categorias);
+        }
+    }
+}
