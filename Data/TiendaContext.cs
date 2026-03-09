0957268c3ea1fecf07cb17820e250a0d681767a1
Lab Simulacion 1 <Lab Simulacion 1@EQUIPO-214>
2026-03-09 17:50:56 -0500

    nuevo y de mas

---
+++ Data/TiendaContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TiendaVirtualReyes.Models;
+
+
+namespace TiendaVirtualReyes.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Producto> productos { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+        public DbSet<Usuario> usuarios { get; set; }
+
+    }
+}
