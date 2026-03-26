267e176bfcfab4c91c96d839fb3b7e175c168b33
AlanR. <alansebas2104@gmail.com>
2026-03-26 16:49:03 -0500

    algo nuevo pero en el controller le puse algo de mas al edit

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -20,5 +20,56 @@ namespace TiendaVirtualReyes.Controllers
 
             return View(productos);
         }
+
+        //formulario crear
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //guardar productos
+        [HttpPost]
+        public IActionResult Create(Producto producto)
+        {
+            _context.productos.Add(producto);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //formulario editar
+        public IActionResult Edit(int id)
+        {
+            // 1. Buscas el producto que vas a editar
+            var producto = _context.productos.Find(id);
+
+            // 2. ¡ESTA ES LA LÍNEA CLAVE! 
+            // Debes traer las categorías de la DB para que el ViewBag no sea nulo
+            ViewBag.Categorias = _context.categorias.ToList();
+
+            // 3. Envías el producto a la vista
+            return View(producto);
+        }
+
+        //actualizar producto 
+        [HttpPost]
+        public IActionResult Edit(Producto producto)
+        {
+            _context.productos.Update(producto);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //Eliminar producto 
+        public IActionResult Delete (int id)
+        {
+            var produco = _context.productos.Find(id);
+
+            _context.productos.Remove(produco);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
